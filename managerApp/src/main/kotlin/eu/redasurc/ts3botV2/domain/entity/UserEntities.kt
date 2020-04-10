8dab98494ce00b3ade82de3a8ca360c809347a07
Redasurc <1006381+Redasurc@users.noreply.github.com>
2020-04-10 21:45:57 +0200

    TS UUID is now unique

--- managerApp/src/main/kotlin/eu/redasurc/ts3botV2/domain/entity/UserEntities.kt
+++ managerApp/src/main/kotlin/eu/redasurc/ts3botV2/domain/entity/UserEntities.kt
@@ -67,6 +67,7 @@ enum class TokenType {
 @Audited
 @EntityListeners(AuditingEntityListener::class)
 class TS3Identity(
+        @Column(unique = true)
         val uuid: String,
 
         @ManyToOne
